db15ddbdb0ddfe529cce691ee2e6001ebd3f78ce
Douglas Little <douglas.a.little@gmail.com>
2020-12-13 17:26:07 -0500

    MVP

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { findByTestId, screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const { getByText } = render(<CheckoutForm />);
+    getByText(/checkout form/i);
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+    const { getByText, findByTestId } = render(<CheckoutForm />);
+    const button = screen.getByRole("button");
+
+    userEvent.click(button);
+    const success = await findByTestId(/successmessage/i);
+    expect(success.toExist);
+});
